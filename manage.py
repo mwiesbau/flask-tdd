c9984150cb3f505c4aa6293318c3c1289932741b
Mathias Wiesbauer <mathias.wiesbauer@gmail.com>
2019-10-09 20:24:57 -0400

    finished deployment to heroku

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
+
 from flask.cli import FlaskGroup
 from project import create_app, db
-from project.api.models import User\
+from project.api.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -9,6 +10,12 @@ cli = FlaskGroup(create_app=create_app)
 def recreate_db():
   db.drop_all()
   db.create_all()
+  db.session.commit()
+
+@cli.command('seed_db')
+def seed_db():
+  db.session.add(User(username="michael", email="hermanmu@gmail.com"))
+  db.session.add(User(username="michaelherman", email="michael@mherman.org"))
   db.session.commit()
 
 
--- project/__init__.py
+++ project/__init__.py
@@ -1,28 +1,27 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-api = Api(app)
 
-# set config
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
 
 db = SQLAlchemy()
 
 
-def create_app(script_info = None):
+def create_app(script_info=None):
   app = Flask(__name__)
+  #app.register_blueprint(users_blueprint)
 
-  app.settings = os.getenv('APP_SETTINGS')
+  app_settings = os.getenv('APP_SETTINGS')
   app.config.from_object(app_settings)
+
 
   db.init_app(app)
 
   from project.api.ping import ping_blueprint
   app.register_blueprint(ping_blueprint)
+
+  from project.api.users import users_blueprint
+  app.register_blueprint(users_blueprint)
 
   @app.shell_context_processor
   def ctx():
